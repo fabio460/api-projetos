6f2f2d98ebc43609d26ab74071ef237d65ba9fa0
fabio460 <dasilvadeoliveirafabio@gmail.com>
2021-10-27 18:37:30 -0300

    teste de deploy 3

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://meuBanco:fabio2020@cluster0.w2xad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+const db = mongoose.connection;
+db.on("err",err=>console.error(err))
+db.once("open",()=>{console.log("conectado no banco de dados")})
+
 
 app.get('/',(req,res)=>{
     res.send('teste')
