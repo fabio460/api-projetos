e56636e04004883323275e0ae15035d2d8ae8b0e
fabio460 <dasilvadeoliveirafabio@gmail.com>
2021-10-27 19:38:13 -0300

    teste 6

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3001
-<<<<<<< HEAD
 app.use(express.static('arquivos'));
 
 
 //conexão
-=======
->>>>>>> parent of 286322d (teste de deploy 5)
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
@@ -18,7 +15,6 @@ const db = mongoose.connection;
 db.on("err",err=>console.error(err))
 db.once("open",()=>{console.log("conectado no banco de dados")})
 
-<<<<<<< HEAD
 //criando tabela no mongo
 const schema = mongoose.Schema({
     titulo:'string',
@@ -32,38 +28,16 @@ app.use(express.json());
 
 //config multer
 
-const multer = require('multer');
-const path = require('path');
-const crypo = require('crypto');
-const config = {
-    storage:multer.diskStorage({
-        destination: (req,file,cb)=>{
-          cb(null,path.resolve(__dirname,"arquivos"))
-        },
-        filename: (req,file,cb)=>{
-            crypo.randomBytes(8,(err,hash)=>{
-                const fileName = `${hash.toString('hex')}-${file.originalname}`;
-                cb(null,fileName);
-            })
-        }
-    })
-}
-
 
 
 
 app.get('/',async(req,res)=>{
   const p = await projeto.find();
   res.json(p)
-=======
-
-app.get('/',(req,res)=>{
-    res.send('teste')
->>>>>>> parent of 286322d (teste de deploy 5)
 })
 
 
-app.post('/',multer(config).single('imagem'),(req,res)=>{
+app.post('/',(req,res)=>{
      
 })
 app.delete('/',(req,res)=>{
